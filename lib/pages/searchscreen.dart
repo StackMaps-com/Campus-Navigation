c77e2d2583e73c2edb204d8639d81655e2d51778
kiranmai2005 <114876920+kiranmai2005@users.noreply.github.com>
2025-06-24 20:33:09 +0530

    Create searchscreen.dart

---
+++ lib/pages/searchscreen.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Set Destination", style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: const Color(0xFF6C11FF),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            buildTextField("From", Icons.my_location),
+            const SizedBox(height: 20),
+            buildTextField("To", Icons.location_on),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(String hint, IconData icon) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hint,
+        prefixIcon: Icon(icon),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        filled: true,
+        fillColor: Colors.grey[200],
+      ),
+    );
+  }
+}
